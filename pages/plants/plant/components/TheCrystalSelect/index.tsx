d0692414a2c823ba12b9d05ebc0e803a3cf0965f
felipe gomez <ggfelipegc@gmail.com>
2023-06-07 14:18:46 -0500

    refactor add new logic to show it

--- pages/plants/plant/components/TheCrystalSelect/index.tsx
+++ pages/plants/plant/components/TheCrystalSelect/index.tsx
@@ -3,22 +3,66 @@ import Image from "next/image";
 import Link from "next/link";
 import zodiac from "./zodiac";
 import vibration from "./assets/vibration.svg";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Crystal } from "../../../../../types";
 
-type Crystal = {
-  name: string;
-  properties: Array<string>;
-  vibration: number;
-  zodiac: Array<string>;
-  imageCrystal: string;
-};
+export default function TheCrystalSelect() {
+  const router = useRouter();
+  const [crystal, setCrystal] = useState<Crystal>(
+    {
+      id: 1,
+      name: "agate",
+      description: "",
+      vibration: 7,
+      benefits: [
+        "estabilización y puesta a tierra cuando estás abrumado",
+        "apoya una conexión profunda con la Tierra y la naturaleza",
+        "ayuda a aceptarte y amarte tal como eres"
+      ],
+      properties: [
+        "conexión con la tierra",
+        "energia fisica",
+        "confianza"
+      ],
+      zodiac: [
+        "geminis",
+        "virgo"
+      ],
+      planets: [
+        "luna"
+      ],
+      elements: [
+        "tierra"
+      ],
+      chakras: [
+        "root chakra"
+      ],
+      imageCrystal: "https://firebasestorage.googleapis.com/v0/b/simple-a5eec.appspot.com/o/crystalsPoints%2Fagate.webp?alt=media&token=be20e165-8b12-4679-b39c-e508f7990ce4",
+      imageGemstone: "https://firebasestorage.googleapis.com/v0/b/simple-a5eec.appspot.com/o/crystalsGemstones%2Fagate.webp?alt=media&token=30d51e3b-038a-4374-860a-263074bd29ff",
+    }
+  );
 
-export default function TheCrystalSelect(crystal: Crystal) {
+  useEffect(() => {
+    async function fetchData() {
+      const id = router.query["crystal"] || 1;
+      const url = `https://plants-api-production.up.railway.app/api/v1/crystals/${id}`;
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setCrystal(data);
+      } catch (error) {
+        
+      }
+    }
+    fetchData();
+  },[router])
   return (
     <section className={styles.crystalCard}>
       <Image
         className={styles.crystalImage}
         src={crystal.imageCrystal}
-        alt={`Image ${crystal.name} Double point Crystall`}
+        alt={`Image ${crystal.name} Double point Crystal`}
         width={150}
         height={150}
       />
@@ -46,15 +90,15 @@ export default function TheCrystalSelect(crystal: Crystal) {
           </Link>
         </div>
         <ul className={styles.properties}>
-          {crystal.properties.map((c) => {
-            return <li>{c}</li>;
+          {crystal.properties.map((c, ci:number) => {
+            return <li key={ci}>{c}</li>;
           })}
         </ul>
         <ul className={styles.zodiac}>
-          {zodiac.map((z) => {
+          {zodiac.map((z, zi:number) => {
             if (crystal.zodiac.includes(z.name)) {
               return (
-                <li>
+                <li key={zi}>
                   <Image src={z.icon} alt={z.alt} width={48} height={48} />
                 </li>
               );
