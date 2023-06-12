3157f9372d1dabb518b0e983e5c4d85270d7b9fc
felipe gomez <ggfelipegc@gmail.com>
2023-06-12 13:25:21 -0500

    refactor change logic add to cart

--- pages/plants/plant/[id].tsx
+++ pages/plants/plant/[id].tsx
@@ -3,7 +3,7 @@ import ThePotSelector from "./components/ThePotSelector";
 import TheCrystalSelector from "./components/TheCrystalSelector";
 import ThePlantView from "./components/ThePlantView";
 import { useRouter } from "next/router";
-import { Crystal, PlantStock } from "../../../types";
+import { Cart, Crystal, PlantStock } from "../../../types";
 import { useEffect, useState } from "react";
 
 const Plant = (props: { plant: PlantStock }) => {
@@ -13,18 +13,36 @@ const Plant = (props: { plant: PlantStock }) => {
   const [renderPot, setRenderPot] = useState("https://firebasestorage.googleapis.com/v0/b/simple-a5eec.appspot.com/o/potsImage/doisu-render.webp?alt=media&token=289ff965-53e9-4a78-b3c2-02edfb573c57");
 
   const addToCart = () => {
-    let item = {
+    let oldCart: Cart[] = [];
+    let newItem: Cart = {
       plant: props.plant.id,
       crystal: +(router.query["crystal"] || 1),
-      pot: +(router.query["pot"] || 1)
+      pot: +(router.query["pot"] || 1),
+      quantity: 1,
     }
-    let newCart = [];
+    let check = null;
     if (localStorage.getItem("cart")) {
-      let actualCart = localStorage.getItem("cart") || "";
-      newCart = JSON.parse(actualCart);
+      let actualCart: string = localStorage.getItem("cart") || "";
+      oldCart = JSON.parse(actualCart);
+      check = oldCart.find((item: Cart) => {
+        return (item.plant === newItem.plant &&
+          item.crystal === newItem.crystal &&
+          item.pot === newItem.pot)
+      })
     }
-    newCart.push(item);
-    localStorage.setItem("cart", JSON.stringify(newCart));
+    if (check) {
+      oldCart = oldCart.map((item: Cart) => {
+        if (item.plant === newItem.plant &&
+          item.crystal === newItem.crystal &&
+          item.pot === newItem.pot) {
+          item.quantity++
+        }
+        return item;
+      })
+    } else {  
+      oldCart.push(newItem);
+    }
+    localStorage.setItem("cart", JSON.stringify(oldCart));
     router.push("/cart");
   }
   const handlerPot = (pot: string) => {
