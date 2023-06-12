ea7e84c74811be53caa938b068dbbe753fe29340
felipe gomez <ggfelipegc@gmail.com>
2023-06-12 13:31:18 -0500

    feat add composition picture and quantity logic

--- pages/cart/components/TheProductCard/index.tsx
+++ pages/cart/components/TheProductCard/index.tsx
@@ -5,23 +5,8 @@ import styles from "./style.module.scss";
 import edit from "./assets/edit.svg";
 import clean from "./assets/delete.svg";
 import TheQuantityCard from "../TheQuantityCard";
+import { Cart, Product } from "../../../../types";
 
-type Product = {
-  id: number;
-  name: string;
-  idPot: number;
-  pot: string;
-  idCrystal: number;
-  crystal: string;
-  price: number;
-  imagePlant: string;
-  imageCrystal: string;
-  quantity: number;
-};
-
-const Quantity = () => {
-  return <div></div>;
-};
 
 const formatter = new Intl.NumberFormat("es-CO", {
   style: "currency",
@@ -29,14 +14,14 @@ const formatter = new Intl.NumberFormat("es-CO", {
   maximumFractionDigits: 0,
 });
 
-export default function TheProductCard(product: Product) {
-  const [total, setTotal] = useState(product.price * product.quantity);
-  const [quantity, setQuantity] = useState(product.quantity);
+export default function TheProductCard(props: { product: Product, delete(id: number, idPot: number, idCrystal: number): void }) {
+  const [total, setTotal] = useState(props.product.price * props.product.quantity);
+  const [quantity, setQuantity] = useState(props.product.quantity);
 
   const handleSubstract = () => {
     if (quantity > 1) {
       setQuantity(quantity - 1);
-      setTotal(quantity * product.price);
+      setTotal(quantity * props.product.price);
     }
   };
   const handleAdd = () => {
@@ -44,30 +29,64 @@ export default function TheProductCard(product: Product) {
       setQuantity(quantity + 1);
     }
   };
-  useEffect(()=>{
-  setTotal(quantity * product.price);
+
+  useEffect(() => {
+    let oldCart: Cart[] = [];
+    let newItem: Cart = {
+      plant: props.product.id,
+      crystal: props.product.idCrystal,
+      pot: props.product.idPot,
+      quantity: quantity,
+    }
+    if (localStorage.getItem("cart")) {
+      let actualCart: string = localStorage.getItem("cart") || "";
+      oldCart = JSON.parse(actualCart);
+      oldCart = oldCart.map((item: Cart) => {
+        if (item.plant === newItem.plant &&
+          item.crystal === newItem.crystal &&
+          item.pot === newItem.pot) {
+          item.quantity = quantity;
+        }
+        return item;
+      })
+      localStorage.setItem("cart", JSON.stringify(oldCart));
+    }
+  },[quantity,props])
+
+  useEffect(() => {
+    setTotal(quantity * props.product.price);
   })
   return (
     <div className={styles.productCard}>
       <div className={styles.images}>
+        <div className={styles.picture}>
+          <Image
+            fill
+            src={props.product.imagePlant}
+            alt={`image plant ${props.product.name}`}
+            sizes="100%"
+          />
+          <Image
+            fill
+            src={props.product.renderPot}
+            alt={`image pot ${props.product.potName}`}
+            sizes="100%"
+
+          />
+        </div>
+        
         <Image
-          src={product.imagePlant}
-          alt={`image plant ${product.name}`}
-          width={200}
-          height={200}
-        />
-        <Image
-          src={product.imageCrystal}
-          alt={`image crystal ${product.crystal}`}
+          src={props.product.imageCrystal}
+          alt={`image crystal ${props.product.crystalName}`}
           width={60}
           height={60}
         />
       </div>
       <div className={styles.details}>
-        <h1>{product.name.toLowerCase()}</h1>
-        <p>Matera Céramica {product.pot}</p>
-        <p>Cristal {product.crystal}</p>
-        <div className={styles.pill}>{formatter.format(product.price)}</div>
+        <h1>{props.product.name.toLowerCase()}</h1>
+        <p>Matera Céramica {props.product.potName}</p>
+        <p>Cristal {props.product.crystalName}</p>
+        <div className={styles.pill}>{formatter.format(props.product.price)}</div>
       </div>
       <TheQuantityCard
         quantity={quantity}
@@ -76,12 +95,14 @@ export default function TheProductCard(product: Product) {
       />
       <div className={styles.info}>
         <div className={styles.actions}>
-          <Link
-            href={`plants/product/${product.id}?crystal=${product.idCrystal}&pot=${product.idPot}`}
+          {/* <Link
+            href={`plants/plant/${props.product.id}?crystal=${props.product.idCrystal}&pot=${props.product.idPot}`}
           >
             <Image src={edit} width={48} height={48} alt="icon edit" />
-          </Link>
-          <Image src={clean} width={48} height={48} alt="icon delete" />
+          </Link> */}
+          <button onClick={() => props.delete(props.product.id, props.product.idPot, props.product.idCrystal)}>
+            <Image src={clean} width={48} height={48} alt="icon delete" />
+          </button>
         </div>
         <div className={styles.prices}>
           <p>SubTotal</p>
