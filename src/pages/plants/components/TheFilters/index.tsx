10ac48e43538274f3afe0c4ce1dd01adb30472b1
felipe gomez <ggfelipegc@gmail.com>
2023-07-02 11:54:53 -0500

    fix replace event

--- src/pages/plants/components/TheFilters/index.tsx
+++ src/pages/plants/components/TheFilters/index.tsx
@@ -93,7 +93,7 @@ export default function TheFilters(props: { params: PlantsQParams }) {
           species?.map((item: string, i: number) => {
             return (
               <label htmlFor={item} className={styles.item} key={i}>
-                <input type="radio" name="species" id={item} onClick={() => setFilter("species", item)} checked={props.params.species == item} />
+                <input type="radio" name="species" id={item} onChange={() => setFilter("species", item)} checked={props.params.species == item} />
                 <label htmlFor={item}></label>
                 <span>{item}</span>
               </label>
@@ -101,7 +101,7 @@ export default function TheFilters(props: { params: PlantsQParams }) {
           })
         }
         <label htmlFor="speciesAll" className={styles.item} key="speciesAll">
-          <input type="radio" name="species" id="speciesAll" checked={props.params.species == null} onClick={() => setFilter("species", "todas")} />
+          <input type="radio" name="species" id="speciesAll" checked={props.params.species == null} onChange={() => setFilter("species", "todas")} />
           <label htmlFor="speciesAll"></label>
           <span>todas</span>
         </label>
@@ -109,22 +109,22 @@ export default function TheFilters(props: { params: PlantsQParams }) {
       <div>
         <h1>Riego</h1>
         <label htmlFor="abundante" className={styles.item}>
-          <input type="radio" name="irrigation" id="abundante" onClick={() => setFilter("irrigation", "abundante")} checked={props.params.irrigation == "abundante"} />
+          <input type="radio" name="irrigation" id="abundante" onChange={() => setFilter("irrigation", "abundante")} checked={props.params.irrigation == "abundante"} />
           <label htmlFor="abundante"></label>
           <span>abundante</span>
         </label>
         <label htmlFor="moderado" className={styles.item}>
-          <input type="radio" name="irrigation" id="moderado" onClick={() => setFilter("irrigation", "moderado")} checked={props.params.irrigation == "moderado"} />
+          <input type="radio" name="irrigation" id="moderado" onChange={() => setFilter("irrigation", "moderado")} checked={props.params.irrigation == "moderado"} />
           <label htmlFor="moderado"></label>
           <span>moderado</span>
         </label>
         <label htmlFor="escaso" className={styles.item}>
-          <input type="radio" name="irrigation" id="escaso" onClick={() => setFilter("irrigation", "escaso")} checked={props.params.irrigation == "escaso"} />
+          <input type="radio" name="irrigation" id="escaso" onChange={() => setFilter("irrigation", "escaso")} checked={props.params.irrigation == "escaso"} />
           <label htmlFor="escaso"></label>
           <span>escaso</span>
         </label>
         <label htmlFor="irrigationAll" className={styles.item}>
-          <input type="radio" name="irrigation" id="irrigationAll" onClick={() => setFilter("irrigation", "todas")} checked={props.params.irrigation == null} />
+          <input type="radio" name="irrigation" id="irrigationAll" onChange={() => setFilter("irrigation", "todas")} checked={props.params.irrigation == null} />
           <label htmlFor="irrigationAll"></label>
           <span>todas</span>
         </label>
@@ -132,28 +132,28 @@ export default function TheFilters(props: { params: PlantsQParams }) {
       <div>
         <h1>Iluminación</h1>
         <label htmlFor="pleno sol" className={styles.item}>
-          <input type="radio" name="light" id="pleno sol" onClick={() => setFilter("light", "pleno sol")} checked={props.params.light == "pleno sol"} />
+          <input type="radio" name="light" id="pleno sol" onChange={() => setFilter("light", "pleno sol")} checked={props.params.light == "pleno sol"} />
           <label htmlFor="pleno sol"></label>
           <span>pleno sol</span>
         </label>
         <label htmlFor="sol parcial" className={styles.item}>
-          <input type="radio" name="light" id="sol parcial" onClick={() => setFilter("light", "sol parcial")} checked={props.params.light == "sol parcial"} />
+          <input type="radio" name="light" id="sol parcial" onChange={() => setFilter("light", "sol parcial")} checked={props.params.light == "sol parcial"} />
           <label htmlFor="sol parcial"></label>
           <span>sol parcial</span>
         </label>
         <label htmlFor="sombra parcial" className={styles.item}>
-          <input type="radio" name="light" id="sombra parcial" onClick={() => setFilter("light", "sombra parcial")} checked={props.params.light == "sombra parcial"} />
+          <input type="radio" name="light" id="sombra parcial" onChange={() => setFilter("light", "sombra parcial")} checked={props.params.light == "sombra parcial"} />
           <label htmlFor="sombra parcial"></label>
           <span>sombra parcial</span>
         </label>
 
         <label htmlFor="sombra completa" className={styles.item}>
-          <input type="radio" name="light" id="sombra completa" onClick={() => setFilter("light", "sombra completa")} checked={props.params.light == "sombra completa"} />
+          <input type="radio" name="light" id="sombra completa" onChange={() => setFilter("light", "sombra completa")} checked={props.params.light == "sombra completa"} />
           <label htmlFor="sombra completa"></label>
           <span>sombra completa</span>
         </label>
         <label htmlFor="lightAll" className={styles.item} key="todas">
-          <input type="radio" name="light" id="lightAll" checked={props.params.light == null} onClick={() => setFilter("light", "todas")} />
+          <input type="radio" name="light" id="lightAll" checked={props.params.light == null} onChange={() => setFilter("light", "todas")} />
           <label htmlFor="lightAll"></label>
           <span>todas</span>
         </label>
@@ -161,17 +161,17 @@ export default function TheFilters(props: { params: PlantsQParams }) {
       <div>
         <h1>Uso</h1>
         <label htmlFor="interior" className={styles.item}>
-          <input type="radio" name="zone" id="interior" onClick={() => setFilter("zone", "interior")} checked={props.params.zone == "interior"} />
+          <input type="radio" name="zone" id="interior" onChange={() => setFilter("zone", "interior")} checked={props.params.zone == "interior"} />
           <label htmlFor="interior"></label>
           <span>interior</span>
         </label>
         <label htmlFor="exterior" className={styles.item}>
-          <input type="radio" name="zone" id="exterior" onClick={() => setFilter("zone", "exterior")} checked={props.params.zone == "exterior"} />
+          <input type="radio" name="zone" id="exterior" onChange={() => setFilter("zone", "exterior")} checked={props.params.zone == "exterior"} />
           <label htmlFor="exterior"></label>
           <span>exterior</span>
         </label>
         <label htmlFor="zoneAll" className={styles.item} key="todas">
-          <input type="radio" name="zone" id="zoneAll" checked={props.params.zone == null} onClick={() => setFilter("zone", "todas")} />
+          <input type="radio" name="zone" id="zoneAll" checked={props.params.zone == null} onChange={() => setFilter("zone", "todas")} />
           <label htmlFor="zoneAll"></label>
           <span>todas</span>
         </label>
