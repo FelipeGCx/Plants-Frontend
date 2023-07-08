11b1e4e715465656385374fbc0c8f45068335d29
felipe gomez <ggfelipegc@gmail.com>
2023-07-08 14:23:15 -0500

    delete comment unused code

--- src/components/commonCrystals/components/TheCrystalView/index.tsx
+++ src/components/commonCrystals/components/TheCrystalView/index.tsx
@@ -24,47 +24,6 @@ export default function TheCrystalView(props: {
   return (
     <section className={styles.view}>
       <ul className={styles.items}>
-        {/* {props.crystals.map((crystal: CrystalFavorite, i: number) => {
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-          const [order, setOrder] = useState((props.totalItems - i) * -1);
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-          const [display, setDisplay] = useState("none");
-          function getNewOrder(oldOrder: number, cols: number) {
-            let n = oldOrder;
-            let flag = true;
-            while (flag) {
-              if (n <= cols) {
-                flag = false;
-              } else {
-                n -= cols;
-              }
-            }
-            setOrder(order - n);
-          }
-          return (
-            <>
-              <TheCrystalCard key={i}
-                crystal={crystal}
-                idx={i}
-                totalItems={props.totalItems}
-                order={order}
-                onFavorite={function (): void {
-                  throw new Error("Function not implemented.");
-                }}
-                onNewOrder={function (oldOrder: number): void {
-                  getNewOrder(oldOrder, 5)
-                }}
-                onDisplay={function (display: string): void {
-                  setDisplay(display);
-                }}
-                onOrder={function (order: number): void {
-                  setOrder(order);
-                }} />
-              <TheCrystalPropertiesCard crystal={crystal} order={order} display={display} />
-              <TheCrystalBenefitsCard crystal={crystal} order={order} display={display} />
-            </>
-          );
-        })} */}
         {props.crystals.map((crystal: CrystalFavorite) => {
           return (
             <TheCrystalCard
