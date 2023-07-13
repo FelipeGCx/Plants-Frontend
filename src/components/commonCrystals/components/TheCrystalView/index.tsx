5739373a674c79fb692d8bb4eb1e55a5ed66ce0f
felipe gomez <ggfelipegc@gmail.com>
2023-07-13 15:46:26 -0500

    feat add show details card when search

--- src/components/commonCrystals/components/TheCrystalView/index.tsx
+++ src/components/commonCrystals/components/TheCrystalView/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./style.module.scss";
 import { CrystalFavorite } from "../../../../types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import TheCrystalCard from "./components/TheCrystalCard";
 import TheCrystalDetails from "./components/TheCrystalDetails";
 
@@ -10,6 +11,7 @@ export default function TheCrystalView(props: {
 }) {
   const [crystal, setCrystal] = useState<CrystalFavorite>();
   const [display, setDisplay] = useState("none");
+  const router = useRouter();
   const handlerDisplay = (display: string, crystalId: number) => {
     console.table({
       display: display,
@@ -21,6 +23,20 @@ export default function TheCrystalView(props: {
     );
     if (crystalSelected) setCrystal(crystalSelected);
   };
+  useEffect(() => {
+    const name = router.query["name"];
+    if (name) {
+      const crystalSelected = props.crystals.find(
+        (crystal) => crystal.name === name
+      );
+      console.log(crystalSelected);
+      if (crystalSelected) {
+        handlerDisplay("grid", crystalSelected?.id);
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.query]);
+
   return (
     <section className={styles.view}>
       <ul className={styles.items}>
