d4ef7a9f9dd059404ce24e8af499f4fc0d3fab97
felipe gomez <ggfelipegc@gmail.com>
2023-07-18 13:49:52 -0500

    feat add more details to loader

--- src/components/commonPlants/components/TheLoader/index.tsx
+++ src/components/commonPlants/components/TheLoader/index.tsx
@@ -2,10 +2,22 @@ import styles from "./style.module.scss";
 
 export default function Loader() {
   const range = Array.from({ length: 30 }, (_, index) => index + 1);
+  const rangeFilter = Array.from({ length: 5 }, (_, index) => index + 1);
   return (
     <div className={styles.main}>
       <section className={styles.filter}>
-        <div></div>
+        <ul className={styles.items}>
+          {range.map((item: any) => {
+            return (
+              <li key={item} className={styles.card}>
+                <span className={styles.title}></span>
+                {rangeFilter.map((num) => {
+                  return <span key={num} className={styles.tag}></span>;
+                })}
+              </li>
+            );
+          })}
+        </ul>
       </section>
       <section className={styles.view}>
         <div className={styles.navsref}>
