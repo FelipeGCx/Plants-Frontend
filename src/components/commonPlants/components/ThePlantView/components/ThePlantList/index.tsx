ef8cc6174a464891495a1b87c7d81f0bf05eb817
felipe gomez <ggfelipegc@gmail.com>
2023-07-19 15:25:36 -0500

    feat add scroll top

--- src/components/commonPlants/components/ThePlantView/components/ThePlantList/index.tsx
+++ src/components/commonPlants/components/ThePlantView/components/ThePlantList/index.tsx
@@ -1,10 +1,24 @@
 import { PlantFavorite } from "../../../../../../types";
 import ThePlantCard from "./components/ThePlantCard";
 import styles from "./style.module.scss";
+import { useEffect, useRef } from "react";
 
 export default function ThePlantList(props: { plants: PlantFavorite[] }) {
+  const containerRef = useRef<HTMLUListElement>(null);
+  useEffect(() => {
+    console.log("cambi", containerRef);
+    const scroll = () => {
+      if (containerRef.current) {
+        containerRef.current.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+    };
+    scroll();
+  }, [props.plants]);
   return (
-    <ul className={styles.items}>
+    <ul className={styles.items} ref={containerRef}>
       {props.plants.map((plant: PlantFavorite, i: number) => {
         return <ThePlantCard key={i} plant={plant} />;
       })}
