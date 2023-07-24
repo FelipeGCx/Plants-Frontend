15db83b73438d69af8c50ab9c6114f2bbd5064de
felipe gomez <ggfelipegc@gmail.com>
2023-07-23 19:39:41 -0500

    feat add some atributes inputs

--- src/components/commonCart/commonCheckout/componets/TheFormData/index.tsx
+++ src/components/commonCart/commonCheckout/componets/TheFormData/index.tsx
@@ -8,29 +8,29 @@ export default function TheFormData() {
       </label>
       <label htmlFor="firstname" className={styles.label}>
         <label htmlFor="firstname">nombre*</label>
-        <input type="text" />
+        <input required type="text" name="firstanem" id="firstanem" />
       </label>
       <label htmlFor="lastname" className={styles.label}>
         <label htmlFor="lastname">apellidos*</label>
-        <input type="text" />
+        <input required type="text" name="lastname" id="lastname" />
       </label>
       <label htmlFor="type" className={styles.label}>
         <label htmlFor="type">tipo de documento*</label>
-        <select name="" id="">
+        <select name="type" id="type">
           <option value=""></option>
         </select>
       </label>
       <label htmlFor="document" className={styles.label}>
         <label htmlFor="document">número de documento*</label>
-        <input type="text" />
+        <input required type="number" name="document" id="document" />
       </label>
       <label htmlFor="email" className={styles.label}>
         <label htmlFor="email">correo electronico*</label>
-        <input type="text" />
+        <input required type="text" name="email" id="email" />
       </label>
       <label htmlFor="phone" className={styles.label}>
         <label htmlFor="phone">telefono*</label>
-        <input type="text" />
+        <input required type="number" name="phone" id="phone" />
       </label>
       <label htmlFor="type" className={styles.label}>
         <label htmlFor="type">departamento*</label>
@@ -44,13 +44,13 @@ export default function TheFormData() {
           <option value=""></option>
         </select>
       </label>
-      <label htmlFor="dirección" className={styles.label}>
-        <label htmlFor="dirección">dirección*</label>
-        <input type="text" />
+      <label htmlFor="address" className={styles.label}>
+        <label htmlFor="address">dirección*</label>
+        <input required type="text" name="address" id="address" />
       </label>
       <label htmlFor="postal" className={styles.label}>
         <label htmlFor="postal">código postal (optional)</label>
-        <input type="text" />
+        <input type="text" name="postal" id="postal" />
       </label>
       <label htmlFor="post" className={styles.labelArea}>
         <label htmlFor="post">nota (optional)</label>
