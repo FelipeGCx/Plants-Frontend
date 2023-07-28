66a3c4faeb9a21dd9bbad84e687bd4e600cffa27
felipe gomez <ggfelipegc@gmail.com>
2023-07-28 14:52:47 -0500

    feat add cart context

--- src/pages/cart/checkout/index.tsx
+++ src/pages/cart/checkout/index.tsx
@@ -1,15 +1,19 @@
-// import { useEffect, useState } from "react";
+import { useContext } from "react";
 import TheDetails from "../../../components/commonCart/commonCheckout/componets/TheDetails";
 import TheFormData from "../../../components/commonCart/commonCheckout/componets/TheFormData";
+import cartContext from "../../../contexts/cartContext";
 import styles from "./style.module.scss";
-// import { ProductTicket } from "../../../types";
-export default function TheCheckout() {
 
+export default function TheCheckout() {
+  const { ticketCart } = useContext(cartContext);
+  const handlerSubmit = (e: any) => {
+    console.log(e)
+  }
   return (
-    <form className={styles.main}>
+    <form className={styles.main} onSubmit={ handlerSubmit}>
       <h1>Datos de Facturación</h1>
       <TheFormData />
-      <TheDetails />
+      <TheDetails products={ticketCart} />
     </form>
   );
 }
