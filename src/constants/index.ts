16535bca184f13264e0653a536c0a0d60e776fe8
felipe gomez <ggfelipegc@gmail.com>
2023-08-04 14:02:02 -0500

    feat: add status const

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,20 @@
+//*||||||||||||*\\
+//* Mock Const *\\
+//*||||||||||||*\\
+
+export const PLANT = "plant";
 export const PLANTS = "plants";
 export const PLANTSFAVORITE = "plantsFavorite";
 export const PLANTSSTOCK = "plantsStock";
 export const CRYSTALS = "crystals";
 export const CRYSTALSFAVORITE = "crystalsFavorite";
 export const CRYSTALSSTOCK = "crystalsStock";
+
+//*||||||||||||||*\\
+//* Status Const *\\
+//*||||||||||||||*\\
+
+export const SUCCESS = "success";
+export const ERROR = "error";
+export const WARNING = "warning";
+export const INFO = "info";
