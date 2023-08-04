ebe02f7ba1a3fcf88c8e98dd027775dd6749ab26
felipe gomez <ggfelipegc@gmail.com>
2023-08-04 14:01:09 -0500

    feat: add more response cases

--- src/services/DevelopmentService.ts
+++ src/services/DevelopmentService.ts
@@ -1,15 +1,19 @@
 import { Response } from "../types";
 import { HttpProvider } from "./HttpProvider";
-import { plants, plantsFavorite } from "./mocks";
-import { PLANTS, PLANTSFAVORITE } from "../constants";
+import { plant, plants, plantsFavorite, plantsStock } from "./mocks";
+import { PLANT, PLANTS, PLANTSFAVORITE, PLANTSSTOCK } from "../constants";
 
 export class DevelopmentService implements HttpProvider {
   async getRequest(url: string): Promise<Response> {
     switch (url) {
+      case PLANT:
+        return plant;
       case PLANTS:
         return plants;
       case PLANTSFAVORITE:
         return plantsFavorite;
+      case PLANTSSTOCK:
+        return plantsStock;
       default:
         return {
           status: "error",
