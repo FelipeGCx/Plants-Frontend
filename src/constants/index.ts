593eebea3c7b38271795bf5dff3f5a8072cbd6ed
felipe gomez <ggfelipegc@gmail.com>
2023-08-04 11:48:28 -0500

    feat: add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const PLANTS = "plants";
+export const PLANTSFAVORITE = "plantsFavorite";
+export const PLANTSSTOCK = "plantsStock";
+export const CRYSTALS = "crystals";
+export const CRYSTALSFAVORITE = "crystalsFavorite";
+export const CRYSTALSSTOCK = "crystalsStock";
