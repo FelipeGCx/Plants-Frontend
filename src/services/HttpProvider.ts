bf43d0e8ad75966f67b93db58c4ffc8095fa3c0c
felipe gomez <ggfelipegc@gmail.com>
2023-08-05 16:41:58 -0500

    fix: fixup imports

--- src/services/HttpProvider.ts
+++ src/services/HttpProvider.ts
@@ -1,7 +1,7 @@
-import { Request } from "../types";
+import { Response } from "../types";
 
 export abstract class HttpProvider {
-  abstract getRequest(url: string): Promise<Request>;
+  abstract getRequest(url: string): Promise<Response>;
   //   abstract postRequest(params: string);
   //   abstract putRequest(params: string);
   //   abstract deleteRequest(params: string);
--- src/services/ProductionService.ts
+++ src/services/ProductionService.ts
@@ -1,8 +1,8 @@
-import { Request } from "../types";
+import { Response } from "../types";
 import { HttpProvider } from "./HttpProvider";
 
 export class ProductionService implements HttpProvider {
-  async getRequest(url: string): Promise<Request> {
+  async getRequest(url: string): Promise<Response> {
     const response = await fetch(url);
     return await response.json();
   }
