4ba76a30ac5c9d3db31bb263741871ec5ab74f1a
felipe gomez <ggfelipegc@gmail.com>
2023-08-12 13:33:45 -0500

    fix: fixup name type

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -3,13 +3,13 @@ import TheProductCard from "../../components/commonCart/components/TheProductCar
 import TheProductList from "../../components/commonCart/components/TheProductList";
 import styles from "./style.module.scss";
 import { useContext } from "react";
-import { CartDrop, Product, Cart } from "../../types";
+import { SimpleCart, Product, Cart } from "../../types";
 import cartContext from "../../contexts/cartContext";
 
 export default function TheCart() {
   const { cart, ticketCart, removeItemCart, setItemQuantity } = useContext(cartContext);
   const handlerDelete = (id: number, idPot: number, idCrystal: number) => {
-    const dropItem: CartDrop = {
+    const dropItem: SimpleCart = {
       plant: id,
       pot: idPot,
       crystal: idCrystal,
